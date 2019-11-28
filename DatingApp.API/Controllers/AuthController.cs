e3f3778329363ebf48cdc62ba8384d7768dfade3
pmehrnoosh <58235789+pmehrnoosh@users.noreply.github.com>
2019-11-28 23:22:02 +0330

    initial

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController :ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+           userForRegisterDto.Username =userForRegisterDto.Username.ToLower();
+            if(await _repo.UserExist(userForRegisterDto.Username)) 
+            return BadRequest("Username already exist");
+
+            var userToCreate= new User
+            {
+                Username=userForRegisterDto.Username
+            };
+            var createdUser= await _repo.Register(userToCreate,userForRegisterDto.Password);
+            return StatusCode(201);
+        }
+    }
+}
---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController: ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context= context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values=await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+        // public ActionResult<IEnumerable<string>> Get(){
+        //     //throw new Exception("Test exception");
+        //     return new string[]{"value1","value2"};
+        // }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var values= await _context.Values.FirstOrDefaultAsync(x=>x.Id==id);
+            return Ok(values);
+        }
+        // public ActionResult<string> Get(int id){
+        //     return "value";
+        // }
+    }
+}
